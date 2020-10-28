3e64ce395553ab156dc4e7e540d883a93b8816a7
00sanoj00 <00sanoj00@gmail.com>
2020-10-28 18:03:20 +0530

    add new InterstitialAd change example ad unit

--- app/src/main/java/lk/devildeveloper/sanoj/sintolegconvert/AppOpenManager.java
+++ app/src/main/java/lk/devildeveloper/sanoj/sintolegconvert/AppOpenManager.java
@@ -22,7 +22,7 @@ import static androidx.lifecycle.Lifecycle.Event.ON_START;
 
 public class AppOpenManager implements LifecycleObserver, Application.ActivityLifecycleCallbacks{
     private static final String LOG_TAG = "AppOpenManager";
-    private static final String AD_UNIT_ID = "ca-app-pub-6390328241621907/5565612324";
+    private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712";
     private AppOpenAd appOpenAd = null;
     private static boolean isShowingAd = false;
     private long loadTime = 0;
--- app/src/main/java/lk/devildeveloper/sanoj/sintolegconvert/MainActivity.java
+++ app/src/main/java/lk/devildeveloper/sanoj/sintolegconvert/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.Html;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -23,6 +24,13 @@ import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.shashank.sony.fancygifdialoglib.FancyGifDialog;
 import com.shashank.sony.fancygifdialoglib.FancyGifDialogListener;
 
@@ -49,11 +57,21 @@ public class MainActivity extends AppCompatActivity {
     private ACProgressFlower dialog;
     private String linkexample;
     private Button erazers;
-
+    private InterstitialAd mInterstitialAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
 
         dialog = new ACProgressFlower.Builder(this)
                 .direction(ACProgressConstant.DIRECT_ANTI_CLOCKWISE)
@@ -95,12 +113,15 @@ public class MainActivity extends AppCompatActivity {
         imagebuttoncopy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String uni = unicode.getText().toString();
-                String clipiniya = Legacy.getText().toString();
-                ClipboardManager clipbord = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("Text",clipiniya);
-                clipbord.setPrimaryClip(clip);
-                Toast.makeText(getApplicationContext(), "Text Copied", Toast.LENGTH_SHORT).show();
+
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                    copymytext();
+                    mInterstitialAd.loadAd(new AdRequest.Builder().build());
+                }
+
             }
         });
 
@@ -804,7 +825,53 @@ public class MainActivity extends AppCompatActivity {
             dialog.dismiss();
 
         }
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            @Override
+            public void onAdFailedToLoad(LoadAdError adError) {
+                // Code to be executed when an ad request fails.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+                Toast.makeText(getApplicationContext(), "Copying begins when the ad closes", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the interstitial ad is closed.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+                copymytext();
+            }
+        });
 
     }
+    public void copymytext(){
+        String uni = unicode.getText().toString();
+        String clipiniya = Legacy.getText().toString();
+        ClipboardManager clipbord = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Text",clipiniya);
+        clipbord.setPrimaryClip(clip);
+        Toast.makeText(getApplicationContext(), "Text Copied", Toast.LENGTH_SHORT).show();
+    }
+
 }
 
